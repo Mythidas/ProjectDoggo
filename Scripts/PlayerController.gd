0073f7e414b10399ae7dc546f6936083a52b4bcc
Mythidas <root@mythidas.com>
2025-11-25 22:02:31 -0600

    init project

---
+++ Scripts/PlayerController.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@export var move_force: float = 800.0
+@export var max_speed: float = 250.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var input_vector = Vector2.ZERO
+
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	input_vector = input_vector.normalized()
+
+	# Apply movement force
+	if input_vector != Vector2.ZERO:
+		move_and_collide(input_vector * move_force * delta)
